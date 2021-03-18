edc3bd1d27681725b4d80d0d32c00014408790b9
Sina <saghaei@usc.edu>
2021-03-18 05:34:52 -0700

    code

--- Code/run_exp.py
+++ Code/run_exp.py
@@ -11,10 +11,10 @@ samples = [1, 2, 3, 4, 5]
 # FlowOCTReplication.main(["-r", 'compas.csv', "-f", 'compas_enc.csv', "-d", 2, "-t", 600, "-l", 0, "-i", 1, "-c", 1, "-a", 'SP', "-b", 0.01, "-e", 'race_factor', "-g", 2])
 
 
-for s in samples:
+for s in [2]:
     for fairness_type in ["SP","None"]:
         if fairness_type == "None":
-            FlowOCTReplication.main(["-r", "compas.csv", "-f", 'compas_enc.csv', "-d", 2, "-t", 600, "-l", 0, "-i", s, "-c", 1, "-a", fairness_type, "-b", 0, "-e", 'race_factor', "-g", 2])
+            FlowOCTReplication.main(["-r", "compas.csv", "-f", 'compas_enc.csv', "-d", 2, "-t", 1200, "-l", 0, "-i", s, "-c", 1, "-a", fairness_type, "-b", 0, "-e", 'race_factor', "-g", 2])
         else:
             for delta in [0.01, 0.05, 0.1 , 0.2, 0.3]:
-                FlowOCTReplication.main(["-r", "compas.csv", "-f", 'compas_enc.csv', "-d", 2, "-t", 600, "-l", 0, "-i", s, "-c", 1, "-a", fairness_type, "-b", delta, "-e", 'race_factor', "-g", 2])
+                FlowOCTReplication.main(["-r", "compas.csv", "-f", 'compas_enc.csv', "-d", 2, "-t", 1200, "-l", 0, "-i", s, "-c", 1, "-a", fairness_type, "-b", delta, "-e", 'race_factor', "-g", 2])
