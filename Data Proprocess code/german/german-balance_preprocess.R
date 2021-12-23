848d9383957b148d0667d163cb1d5cd9152509ef
Sina <saghaei@usc.edu>
2021-12-22 17:17:08 -0800

    test

--- Data Proprocess code/german/german-balance_preprocess.R
+++ Data Proprocess code/german/german-balance_preprocess.R
@@ -200,6 +200,6 @@ for(Run in c(1,2,3,4,5)){
 
 
 # # Test
-# data_train<- read.csv("/Users/sina/Documents/GitHub/FairStrongTrees/DataSets/german_calibration_1.csv", header = TRUE, sep = ",",na.strings = "",stringsAsFactors = TRUE)
+german_train<- read.csv("/Users/sina/Documents/GitHub/FairStrongTrees/DataSets/KamiranVersion/german-balance_train_calibration_1.csv", header = TRUE, sep = ",",na.strings = "",stringsAsFactors = TRUE)
 # data_train_kamiran <- read.csv("/Users/sina/Documents/GitHub/FairStrongTrees/DataSets/Kamiran Version/german_calibration_1.csv", header = TRUE, sep = ",",na.strings = "",stringsAsFactors = TRUE)
-# data_test<- read.csv("/Users/sina/Documents/GitHub/FairStrongTrees/DataSets/german_test_1.csv", header = TRUE, sep = ",",na.strings = "",stringsAsFactors = TRUE)
+german_test<- read.csv("/Users/sina/Documents/GitHub/FairStrongTrees/DataSets/KamiranVersion/german-balance_test_1.csv", header = TRUE, sep = ",",na.strings = "",stringsAsFactors = TRUE)
