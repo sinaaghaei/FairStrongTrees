33d33d3623a2ac1a7013aecde37c3a81f9a6dd47
Sina <saghaei@usc.edu>
2022-01-10 01:59:35 -0800

    updated figures

---
+++ slurm/FlowOCT_limited-adult-2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sbatch --array 0-99 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 100-199 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 200-299 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 300-399 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 400-499 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 500-599 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 600-699 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 700-799 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 800-899 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 900-999 slurm_FlowOCT_10800_limited-adult-2.sh
+sbatch --array 1000-1104 slurm_FlowOCT_10800_limited-adult-2.sh
